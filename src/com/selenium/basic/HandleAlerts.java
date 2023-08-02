07e79beaf75ed0c2c592b8b8bd17a22adaf280e9
Sahil K <45844195+sahilsingh511@users.noreply.github.com>
2023-08-02 22:08:33 +0530

    Update HandleAlerts.java

--- src/com/selenium/basic/HandleAlerts.java
+++ src/com/selenium/basic/HandleAlerts.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class HandleAlerts {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Genius\\eclipse-workspace\\MiniProject\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
